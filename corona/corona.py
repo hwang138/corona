e0c8e598319e130d1befcab4160b4c3877924ff2
Chris Hwang <chwang@Chriss-MBP.attlocal.net>
2020-04-11 17:46:33 -0500

    Update corona.py

--- corona/corona.py
+++ corona/corona.py
@@ -7,7 +7,7 @@ from corona.src import utils
 class Corona(object):
     def __init__(self):
         self.description = "COVID-19 analysis class"
-        self.corona_gobals = corona_gobals
+        self.globals = corona_gobals
         self.plotting_utils = plotting_utils
         self.utils = utils
         self.pull_latest_data()
@@ -26,48 +26,80 @@ class Corona(object):
 
         print("###############" "\nData Processing" "\n###############")
         self.data = self.data.sort_values(
-            by=[corona_gobals.COUNTRY_COLUMN, corona_gobals.DATE_COLUMN]
+            by=[globals.COUNTRY_COLUMN, globals.DATE_COLUMN]
         )
 
         self._integrate_numbers_by_country(
-            input_column=self.corona_gobals.VALUE_COLUMN_LIST
+            input_column=self.globals.VALUE_COLUMN_LIST
         )
 
     def _integrate_numbers_by_country(self, input_column):
+        """
+        Numeric integration of daily values to cumulative values.
+
+        Parameters
+        ----------
+        input_column: str or [str]
+            columns in self.data of daily values
+
+        Returns
+        -------
+            self.data[f"cumulative_{input_column}"] with int values.
+        """
         print("compute cumulative values ... working ... ", end="")
 
+        # nest str into list or keep as list
         input_column_list = self.utils.generate_list(input_column)
-        new_value_column_list = []
 
-        for input in input_column_list:
-            new_key = f"cumulative_{input}"
-            new_value_column_list.append(new_key)
+        for input_column in input_column_list:
+            new_key = f"cumulative_{input_column}"
+            for country in self.data[globals.COUNTRY_COLUMN].unique():
+                mask = self.data[globals.COUNTRY_COLUMN] == country
 
-            for country in self.data[corona_gobals.COUNTRY_COLUMN].unique():
-                mask = self.data[corona_gobals.COUNTRY_COLUMN] == country
-                self.data.loc[
-                    mask, f"cumulative_{input}"
-                ] = self.utils.integrate_numbers(self.data.loc[mask, input])
+                # use integrate_numbers to add daily values with the same 1-D array length
+                self.data.loc[mask, new_key] = self.utils.integrate_numbers(
+                    self.data.loc[mask, input_column]
+                )
+            # convert to int
             self.data[new_key] = self.data[new_key].astype(int)
-        self.value_column_list = input_column_list + new_value_column_list
         print("done.\n")
 
     def print_latest_case_numbers(self, day_range=3):
+        """
+        Print table of latest case numers
+
+        Parameters
+        ----------
+        day_range: int
+            How many days into the past to include values for.
+
+        Returns
+        -------
+            pandas df
+        """
         print(f"Case and death numbers for the last {day_range} days.")
-        max_date_min_d = self.data[self.corona_gobals.DATE_COLUMN].max() - timedelta(
+
+        # compute dt with - day_range value
+        max_date_min_d = self.data[self.globals.DATE_COLUMN].max() - timedelta(
             days=day_range
         )
-        country_list = self.corona_gobals.COUNTRY_LIST
+
+        country_list = self.globals.COUNTRY_LIST
         return (
+            # subset from data
             self.data.query(
-                f"{self.corona_gobals.DATE_COLUMN} > @max_date_min_d"
-                f" & {self.corona_gobals.COUNTRY_COLUMN} in @country_list"
+                f"{self.globals.DATE_COLUMN} > @max_date_min_d"
+                f" & {self.globals.COUNTRY_COLUMN} in @country_list"
             )
-            .reset_index()
-            .sort_values(by=[self.corona_gobals.COUNTRY_COLUMN, self.corona_gobals.DATE_COLUMN], ascending=False)[
+            .sort_values(
+                by=[self.globals.COUNTRY_COLUMN, self.globals.DATE_COLUMN],
+                ascending=False,
+            )
+            .reset_index()[
+                # extract columns of interest
                 [
-                    self.corona_gobals.DATE_COLUMN,
-                    self.corona_gobals.COUNTRY_COLUMN,
+                    self.globals.DATE_COLUMN,
+                    self.globals.COUNTRY_COLUMN,
                     "cases",
                     "cumulative_cases",
                     "deaths",
@@ -76,24 +108,48 @@ class Corona(object):
             ]
         )
 
-    def plot_data(self, fontsize=14):
+    def plot_data(self, fontsize=14, day_range=None):
+        """
+        Plot self.data
+
+        Parameters
+        ----------
+        fontsize: int
+        day_range: int (default: None)
+            How many days into the past to include values for.
+
+        Returns
+        -------
+
+        """
         initial_y_list = [
             "cases",
             "deaths",
         ]
         cumulative_y_list = ["cumulative_" + elem for elem in initial_y_list]
 
+        # compute dt with - day_range value
+        # default is to plot all time points
+        if day_range is None:
+            max_date_min_d = self.data[self.globals.DATE_COLUMN].min()
+        else:
+            max_date_min_d = self.data[self.globals.DATE_COLUMN].max() - timedelta(
+                days=day_range
+            )
+
         for y_list in [
             cumulative_y_list,
             initial_y_list,
         ]:
-            country_list = self.corona_gobals.COUNTRY_LIST
-
+            country_list = self.globals.COUNTRY_LIST
             self.plotting_utils.plot_time_series(
-                data=self.data.query(f"{self.corona_gobals.COUNTRY_COLUMN} in @country_list"),
-                x="date",
+                data=self.data.query(
+                    f"{self.globals.DATE_COLUMN} > @max_date_min_d"
+                    f"{self.globals.COUNTRY_COLUMN} in @country_list"
+                ),
+                x=self.globals.DATE_COLUMN,
                 y=y_list,
-                ax_factor="countries_and_territories",
+                ax_factor=self.globals.COUNTRY_COLUMN,
                 fontsize=fontsize,
             )
             print("\n\n")
