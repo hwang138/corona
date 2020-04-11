66a4fd0d39c5019e31c0f4cf41bbb80e51458017
Chris Hwang <chwang@Chriss-MBP.attlocal.net>
2020-04-11 14:49:58 -0500

    schedule run: corona_19_visualization.ipynb

--- corona/corona.py
+++ corona/corona.py
@@ -1,5 +1,4 @@
-import copy
-
+from datetime import timedelta
 from corona.src import corona_gobals
 from corona.src import plotting_utils
 from corona.src import utils
@@ -25,70 +24,73 @@ class Corona(object):
             verbose=verbose
         )
 
-        print(
-            "###############"
-            "\nData Processing"
-            "\n###############"
-        )
+        print("###############" "\nData Processing" "\n###############")
         self.data = self.data.sort_values(
             by=[corona_gobals.COUNTRY_COLUMN, corona_gobals.DATE_COLUMN]
         )
-        self._compute_per_capita_values(
-            value_column_list=self.corona_gobals.VALUE_COLUMN_LIST
+
+        self._integrate_numbers_by_country(
+            input_column=self.corona_gobals.VALUE_COLUMN_LIST
         )
-
-        self._integrate_numbers_by_country(input_column=self.value_column_list)
-
-    def _compute_per_capita_values(self, value_column_list):
-        print("compute per capita values ... working ... ", end="")
-        new_value_column_list = copy.copy(value_column_list)
-        for key in value_column_list:
-            new_key = f"{key}_per_capita"
-            self.data[new_key] = self.data.apply(
-                lambda x: x[key] / x[corona_gobals.CAPITA_COLUMN], axis=1
-            )
-            new_value_column_list.append(new_key)
-        self.value_column_list = new_value_column_list
-        print("done.\n")
 
     def _integrate_numbers_by_country(self, input_column):
         print("compute cumulative values ... working ... ", end="")
 
-        input_list = self.utils.generate_list(input_column)
+        input_column_list = self.utils.generate_list(input_column)
+        new_value_column_list = []
 
-        for country in self.data[corona_gobals.COUNTRY_COLUMN].unique():
-            mask = self.data[corona_gobals.COUNTRY_COLUMN] == country
-            for input in input_list:
+        for input in input_column_list:
+            new_key = f"cumulative_{input}"
+            new_value_column_list.append(new_key)
+
+            for country in self.data[corona_gobals.COUNTRY_COLUMN].unique():
+                mask = self.data[corona_gobals.COUNTRY_COLUMN] == country
                 self.data.loc[
                     mask, f"cumulative_{input}"
                 ] = self.utils.integrate_numbers(self.data.loc[mask, input])
+            self.data[new_key] = self.data[new_key].astype(int)
+        self.value_column_list = input_column_list + new_value_column_list
         print("done.\n")
+
+    def print_latest_case_numbers(self, day_range=3):
+        print(f"Case and death numbers for the last {day_range} days.")
+        max_date_min_d = self.data[self.corona_gobals.DATE_COLUMN].max() - timedelta(
+            days=day_range
+        )
+        country_list = self.corona_gobals.COUNTRY_LIST
+        return (
+            self.data.query(
+                f"{self.corona_gobals.DATE_COLUMN} > @max_date_min_d"
+                f" & {self.corona_gobals.COUNTRY_COLUMN} in @country_list"
+            )
+            .reset_index()
+            .sort_values(by=[self.corona_gobals.COUNTRY_COLUMN, self.corona_gobals.DATE_COLUMN], ascending=False)[
+                [
+                    self.corona_gobals.DATE_COLUMN,
+                    self.corona_gobals.COUNTRY_COLUMN,
+                    "cases",
+                    "cumulative_cases",
+                    "deaths",
+                    "cumulative_deaths",
+                ]
+            ]
+        )
 
     def plot_data(self, fontsize=14):
         initial_y_list = [
             "cases",
             "deaths",
         ]
-        per_cap_y_list = [elem + "_per_capita" for elem in initial_y_list]
         cumulative_y_list = ["cumulative_" + elem for elem in initial_y_list]
-        cumulative_per_cap_y_list = [elem + "_per_capita" for elem in cumulative_y_list]
 
         for y_list in [
             cumulative_y_list,
-            cumulative_per_cap_y_list,
             initial_y_list,
-            per_cap_y_list,
         ]:
-            subset_country_list = [
-                "United_States_of_America",
-                "Taiwan",
-                "Italy",
-                "Spain",
-                "Germany",
-                "China",
-            ]
+            country_list = self.corona_gobals.COUNTRY_LIST
+
             self.plotting_utils.plot_time_series(
-                data=self.data.query("countries_and_territories in @subset_country_list"),
+                data=self.data.query(f"{self.corona_gobals.COUNTRY_COLUMN} in @country_list"),
                 x="date",
                 y=y_list,
                 ax_factor="countries_and_territories",
--- corona/src/corona_gobals.py
+++ corona/src/corona_gobals.py
@@ -1,4 +1,12 @@
 COUNTRY_COLUMN = "countries_and_territories"
+COUNTRY_LIST = [
+                "United_States_of_America",
+                "Taiwan",
+                "Italy",
+                "Spain",
+                "Germany",
+                "China",
+            ]
 DATE_COLUMN = "date"
 CAPITA_COLUMN = "pop_data2018"
 VALUE_COLUMN_LIST = ["cases", "deaths"]
