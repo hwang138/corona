ae9f1d92ba6b01d560f6d8eb2f6d07281f02d216
Chris Hwang <chwang@Chriss-MBP.attlocal.net>
2020-04-11 17:58:34 -0500

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -1,1 +1,2 @@
-conda install -y --override-channels -c conda-forge black jupyter matplotlib nb_black nbconvert numpy pandas
+conda install -y --override-channels -c conda-forge black jupyter matplotlib nb_black nbconvert numpy pandas \
+scikit-learn
