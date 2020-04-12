4152e05a6367f13e5e251544dbfc7e6c43eca35d
Chris Hwang <chwang@Chriss-MacBook-Pro.local>
2020-04-12 18:12:29 -0500

    scheduled run: corona_19_visualization.ipynb

--- scripts/run_and_upload.sh
+++ scripts/run_and_upload.sh
@@ -1,6 +1,6 @@
 # Description: Script to run corona_19_visualization.ipynb and upload back to git
 # Author: Chris Hwang
-
+git checkout master
 # use nbconvert to execute the notebook
 jupyter nbconvert --to notebook --execute  ./corona_19_visualization.ipynb
 
