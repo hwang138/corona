8998302d3af384df6959b43b162aa43c1180d9f6
Chris Hwang <chwang@Chriss-MBP.attlocal.net>
2020-04-12 12:02:08 -0500

    add some options to Model.fit()

--- corona/src/model/src/simple_model.py
+++ corona/src/model/src/simple_model.py
@@ -17,20 +17,3 @@ def fit(model_class, X, y):
     model_class.model.fitted_model
     """
     model_class.model.fitted_model = LinearRegression().fit(X=X, y=y)
-
-
-def predict(model_class, X):
-    """
-    Make prediction
-
-    Parameters
-    ----------
-    model_class: obj
-    X: np.ndarray
-        design matrix
-
-    Returns
-    -------
-    np.ndarray of predicion values
-    """
-    return model_class.model.fitted_model.predict(X=X)
